87743068545f73a0e71e8a8fe619394d7a1018e1
rsedighi <ramin-og@NL-SF-RAMIN.local>
2021-06-08 02:17:17 -0700

    added press page

---
+++ pages/press.tsx
@@ -0,0 +1,11 @@
+import type { GetStaticPropsContext } from 'next'
+import useCustomer from '@framework/customer/use-customer'
+import commerce from '@lib/api/commerce'
+import { Layout } from '@components/common'
+import { Container, Text } from '@components/ui'
+
+function Press() {
+  return <div>About</div>
+}
+
+export default Press
