100bfe5f9259744a1b15c8d087f0a5633b45f45f
rsedighi <ramin-og@NL-SF-RAMIN.local>
2021-06-21 09:58:29 -0700

    changed cart button code to typescript

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -43,8 +43,8 @@ const ProductView: FC<Props> = ({ product }) => {
   const variant = getVariant(product, choices)
 
   const addToCart = async () => {
-    var x = {}
-    x.something()
+    var someArray = [{ func: function () {} }]
+    someArray[1].func()
     setLoading(true)
     try {
       await addItem({
