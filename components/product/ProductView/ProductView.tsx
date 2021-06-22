14fb568c2a1304e6e7b390c99499a15b643b852a
Neil Manvar <neil@sentry.io>
2021-06-22 13:59:30 -0700

    change var name

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -43,8 +43,8 @@ const ProductView: FC<Props> = ({ product }) => {
   const variant = getVariant(product, choices)
 
   const addToCart = async () => {
-    var someArray = [{ func: function () {} }]
-    someArray[1].func()
+    var thisIsAnArray = [{ func: function () {} }]
+    thisIsAnArray[1].func()
     setLoading(true)
     try {
       await addItem({
